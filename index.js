01c62b391d4cb714c533132d1bf3953ba50aca25
T L <tom.kennedy.lee@gmail.com>
2018-06-04 14:25:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+kittens.push("Ralph")
+return kittens 
+}
+
+function destructivelyPrependKitten(name) {
+kittens.unshift("Bob")
+  return kittens 
+}
+
+function destructivelyRemoveLastKitten(name) {
+kittens.pop("Garfield")
+return kittens 
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift("Milo")
+  return kittens 
+}
+
+function appendKitten(name, array) {
+  var array = [...kittens, "Broom"]
+  return array 
+}
+
+function prependKitten(name, array) {
+  var array2 = ["Arnold", ...kittens]
+  return array2
+}
+
+function removeLastKitten(array5) {
+return array5.slice(0, array5.length - 1)
+}
